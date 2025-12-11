07f8f75035fb28e40050c29d1ad6a5eaea902f4e
guozr <guozr1997@hotmail.com>
2025-12-11 12:42:18 +0800

    feat(colorscheme): enable transparent background for catppuccin
    WHAT: Enable transparent background in catppuccin colorscheme configuration
    WHY: Improve visual aesthetics by allowing terminal background to show through
    HOW: Add transparent_background = true option to catppuccin setup configuration

--- neovim/nvim/lua/plugins/colorschemes.lua
+++ neovim/nvim/lua/plugins/colorschemes.lua
@@ -2,7 +2,10 @@ return {
   {
     "catppuccin/nvim",
     config = function()
-      require("catppuccin").setup({ flavour = "frappe" })
+      require("catppuccin").setup({
+        flavour = "frappe",
+        transparent_background = true,
+      })
       vim.cmd.colorscheme "catppuccin"
     end,
   },
