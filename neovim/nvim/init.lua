013fd388f9df61e9fd21823062b4750fbfd09472
guozr <guozr1997@hotmail.com>
2025-12-13 14:13:40 +0800

    prompt(neovim): 添加 Markdown 文件自动换行配置
    WHAT: 为 Markdown 文件添加专用的自动换行和导航优化配置
    
    WHY: 提升 Markdown 文件的编辑体验，使长文本更易读且导航更流畅
    
    HOW: 通过 FileType autocmd 为 Markdown 文件设置优化的换行选项（wrap, linebreak, breakindent），配置 80 字符行宽限制，启用拼写检查，并重新映射 j/k/0/$ 键以适应换行模式下的导航

--- neovim/nvim/init.lua
+++ neovim/nvim/init.lua
@@ -34,3 +34,32 @@ require('lazy').setup('plugins', {
     notify = false,
   },
 })
+
+-- Configure line wrap for markdown files with best practices
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'markdown',
+  callback = function()
+    -- Basic wrap settings
+    vim.opt_local.wrap = true
+    vim.opt_local.linebreak = true
+    vim.opt_local.breakindent = true
+
+    -- Optimized break indent
+    vim.opt_local.breakindentopt = 'shift:2,min:20,sbr'
+    vim.opt_local.showbreak = '   ↪ '
+
+    -- Text width and formatting
+    vim.opt_local.textwidth = 80
+    vim.opt_local.formatoptions = 'tcqnj'
+
+    -- Spell checking for markdown
+    vim.opt_local.spell = true
+    vim.opt_local.spelllang = 'en_us'
+
+    -- Improved navigation for wrapped lines
+    vim.keymap.set('n', 'j', 'gj', { buffer = true, silent = true })
+    vim.keymap.set('n', 'k', 'gk', { buffer = true, silent = true })
+    vim.keymap.set('n', '0', 'g0', { buffer = true, silent = true })
+    vim.keymap.set('n', '$', 'g$', { buffer = true, silent = true })
+  end,
+})
