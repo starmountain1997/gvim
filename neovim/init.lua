427c3c9b34eb13d8a13e6b70abe1a522926cbe96
guozr <guozr1997@hotmail.com>
2026-04-27 10:57:42 +0800

    style(lua): configure StyLua with 4-space indentation
    WHAT: Added stylua.toml with Spaces indent_type and 4 indent_width.
    WHY: Align with project convention for 4-space indentation.
    HOW: StyLua now reformats lua files with 4-space indents.

--- neovim/init.lua
+++ neovim/init.lua
@@ -1,45 +1,45 @@
 -- Lazy.nvim bootstrap
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable",
-		lazypath,
-	})
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable",
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- Plugins
 require("lazy").setup({
-	-- Git signs
-	{
-		"lewis6991/gitsigns.nvim",
-		config = function()
-			require("gitsigns").setup({
-				signs = {
-					add = { text = "┃" },
-					change = { text = "┃" },
-					delete = { text = "_" },
-					topdelete = { text = "‾" },
-					changedelete = { text = "~" },
-					untracked = { text = "┆" },
-				},
-				signcolumn = true,
-				numhl = false,
-				linehl = false,
-				word_diff = false,
-				watch_gitdir = {
-					follow_files = true,
-				},
-				auto_attach = true,
-				attach_to_untracked = false,
-				current_line_blame = false,
-				update_debounce = 100,
-				max_file_length = 40000,
-			})
-		end,
-	},
+    -- Git signs
+    {
+        "lewis6991/gitsigns.nvim",
+        config = function()
+            require("gitsigns").setup({
+                signs = {
+                    add = { text = "┃" },
+                    change = { text = "┃" },
+                    delete = { text = "_" },
+                    topdelete = { text = "‾" },
+                    changedelete = { text = "~" },
+                    untracked = { text = "┆" },
+                },
+                signcolumn = true,
+                numhl = false,
+                linehl = false,
+                word_diff = false,
+                watch_gitdir = {
+                    follow_files = true,
+                },
+                auto_attach = true,
+                attach_to_untracked = false,
+                current_line_blame = false,
+                update_debounce = 100,
+                max_file_length = 40000,
+            })
+        end,
+    },
 })
--- neovim/lua/plugins/render-markdown.lua
+++ neovim/lua/plugins/render-markdown.lua
@@ -1,5 +1,5 @@
 return {
-	"MeanderingProgrammer/render-markdown.nvim",
-	ft = { "markdown" },
-	dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
+    "MeanderingProgrammer/render-markdown.nvim",
+    ft = { "markdown" },
+    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
 }
