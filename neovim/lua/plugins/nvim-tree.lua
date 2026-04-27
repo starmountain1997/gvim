37709e0d70ddbbd9589cd13d170b7438d6ce0a8f
guozr <guozr1997@hotmail.com>
2026-04-27 10:57:23 +0800

    style(lua): format code with StyLua
    WHAT: Applied StyLua formatting to lua plugin files and updated pre-commit config.
    WHY: Enforce consistent Lua code style across the neovim configuration.
    HOW: Added official StyLua pre-commit hook using cargo language (requires Rust).

--- neovim/lua/plugins/nvim-tree.lua
+++ neovim/lua/plugins/nvim-tree.lua
@@ -1,10 +1,10 @@
 return {
-  "nvim-tree/nvim-tree.lua",
-  config = function()
-    require("nvim-tree").setup({
-      filters = {
-        custom = { "__pycache__", "%.egg-info" },
-      },
-    })
-  end,
+	"nvim-tree/nvim-tree.lua",
+	config = function()
+		require("nvim-tree").setup({
+			filters = {
+				custom = { "__pycache__", "%.egg-info" },
+			},
+		})
+	end,
 }
